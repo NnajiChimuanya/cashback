61055338df6a153cd538cd33f63c4a44c2d3b313
Nnaji Chimuanya <nnajichimuanyua50@gmail.com>
2022-07-26 15:07:48 -0700

    Set up the router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
