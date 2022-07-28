fadb4fcced7fb5c4cdfbc18c8437237c568111b2
Nnaji Chimuanya <nnajichimuanyua50@gmail.com>
2022-07-28 04:04:40 -0700

    Made changes

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,22 +1,29 @@
 import "./header.css";
 import React from "react";
-import { Search, NotificationAdd } from "@mui/icons-material";
+import { Search, NotificationAdd, BlurOn } from "@mui/icons-material";
 import { Avatar } from "@mui/material";
 
 const Header = () => {
   return (
     <div className="header">
-      <div className="search">
-        <div className="search-container">
-          <Search />
-
-          <input type="text" placeholder="Search" />
-        </div>
+      <div>
+        <BlurOn className="blur-on" />
       </div>
 
       <div className="actions">
-        <Avatar />
-        <NotificationAdd />
+        <div className="search">
+          <div className="search-container">
+            <Search className="search-icon" />
+
+            <input type="text" placeholder="Search" />
+          </div>
+        </div>
+        <div>
+          <NotificationAdd />
+        </div>
+        <div>
+          <Avatar className="avatar" />
+        </div>
       </div>
     </div>
   );
