37dd769b3b44fe460af1aae00a4d109199070c56
Nnaji Chimuanya <nnajichimuanyua50@gmail.com>
2022-07-30 06:08:04 -0700

    Added a barchart

--- src/pages/dashboard/DashboardPage.js
+++ src/pages/dashboard/DashboardPage.js
@@ -9,6 +9,8 @@ import {
   Tooltip,
   Legend,
   ResponsiveContainer,
+  BarChart,
+  Bar,
 } from "recharts";
 import { lineChartData } from "./lineChartData.js";
 
@@ -69,6 +71,19 @@ const DashboardPage = () => {
             <Line type="monotone" dataKey="pv" stroke="#8884d8" />
             <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
           </LineChart>
+        </div>
+      </div>
+      <div className="data-row-2">
+        <div className=" bar-chart">
+          <BarChart width={500} height={250} data={lineChartData}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="pv" fill="#8884d8" />
+            <Bar dataKey="uv" fill="#82ca9d" />
+          </BarChart>
         </div>
       </div>
     </div>
