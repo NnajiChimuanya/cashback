439fd1cc93da46e1748a092cea25b9ab7d4e5aa6
Chimuanya Nnaji <nnajichimuanya50@gmail.com>
2022-07-30 06:35:44 +0400

    worked on the dashboard overview

--- src/pages/dashboard/DashboardPage.js
+++ src/pages/dashboard/DashboardPage.js
@@ -7,6 +7,24 @@ const DashboardPage = () => {
       <div className="dashboard-header">
         <h4> DASHBOARD</h4>
       </div>
+      <div className="overview">
+        <div className="overview-data">
+          <div className="title"> Total Earning</div>
+          <div className="amount">Rp870</div>
+        </div>
+        <div className="overview-data">
+          <div className="title"> Total Bonus</div>
+          <div className="amount">Rp870</div>
+        </div>
+        <div className="overview-data">
+          <div className="title"> Total Cashback</div>
+          <div className="amount">Rp870</div>
+        </div>
+        <div className="overview-data">
+          <div className="title"> Net profit</div>
+          <div className="amount">Rp870</div>
+        </div>
+      </div>
     </div>
   );
 };
