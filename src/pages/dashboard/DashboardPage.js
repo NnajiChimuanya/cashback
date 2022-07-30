6e007039610cd70ef5bee8718bf7c23cb67de923
Chimuanya Nnaji <nnajichimuanya50@gmail.com>
2022-07-30 18:18:24 +0400

    Fixed the mobile responsiveness

--- src/pages/dashboard/DashboardPage.js
+++ src/pages/dashboard/DashboardPage.js
@@ -57,6 +57,7 @@ const DashboardPage = () => {
           </LineChart>
         </div>
         <div className="line-chart">
+          <h4 className="line-chart-title"> Commission/Cashback</h4>
           <LineChart
             width={500}
             height={250}
@@ -74,7 +75,8 @@ const DashboardPage = () => {
         </div>
       </div>
       <div className="data-row-2">
-        <div className=" bar-chart">
+        <div className="bar-chart">
+          <h4 className="bar-chart-title"> Total Clicks</h4>
           <BarChart width={500} height={250} data={lineChartData}>
             <CartesianGrid strokeDasharray="3 3" />
             <XAxis dataKey="name" />
@@ -84,6 +86,47 @@ const DashboardPage = () => {
             <Bar dataKey="pv" fill="#8884d8" />
             <Bar dataKey="uv" fill="#82ca9d" />
           </BarChart>
+        </div>
+        <div className="line-chart">
+          <h4 className="line-chart-title"> Total Users</h4>
+          <LineChart
+            width={500}
+            height={250}
+            data={lineChartData}
+            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="pv" stroke="#8884d8" />
+          </LineChart>
+        </div>
+      </div>
+      <div className="data-row-3">
+        <h4> Quick Links</h4>
+        <div className="quick-links">
+          <div className="quick-links-data">
+            <div className="title"> Total Bonus</div>
+            <div className="amount">Rp870</div>
+          </div>
+          <div className="quick-links-data">
+            <div className="title"> Total Cashback</div>
+            <div className="amount">Rp870</div>
+          </div>
+          <div className="quick-links-data">
+            <div className="title"> Net profit</div>
+            <div className="amount">Rp870</div>
+          </div>
+          <div className="quick-links-data">
+            <div className="title"> Net profit</div>
+            <div className="amount">Rp870</div>
+          </div>
+          <div className="quick-links-data">
+            <div className="title"> Net profit</div>
+            <div className="amount">Rp870</div>
+          </div>
         </div>
       </div>
     </div>
