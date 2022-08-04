96d7e4dda81c2cdeb002dbb34491c0ab6b00fb49
Nnaji Chimuanya <nnajichimuanyua50@gmail.com>
2022-08-04 00:16:42 -0700

    Added the table

--- src/pages/stores/Stores.js
+++ src/pages/stores/Stores.js
@@ -1,7 +1,43 @@
 import React from "react";
 import "./stores.css";
 import { Button } from "@material-ui/core";
-import { Add, FilterAlt } from "@mui/icons-material";
+import { Add, FilterAlt, Loop } from "@mui/icons-material";
+import { DataGrid } from "@mui/x-data-grid";
+
+const columns = [
+  { field: "id", headerName: "ID", width: 70 },
+  { field: "name", headerName: "Name", width: 130 },
+  { field: "cashbackEnabled", headerName: "Cashback Enabled", width: 150 },
+  { field: "cashbackType", headerName: "cashback Type", width: 130 },
+  { field: "cashbackPercent", headerName: "Cashback Percent", width: 130 },
+  { field: "isfeatured", headerName: "Is Featured", width: 100 },
+  { field: "visits", headerName: "visit", width: 80 },
+  { field: "rating", headerName: "Rating", width: 80 },
+  { field: "clicks", headerName: "Clicks", width: 80 },
+  { field: "status", headerName: "status", width: 100 },
+];
+
+const rows = [
+  {
+    id: 1,
+    cashbackEnabled: "Snow",
+    cashbackType: "Jon",
+    cashbackPercent: "80",
+    isFeatured: "true",
+    visits: 500,
+    clicks: 500,
+    rating: 5,
+    status: "jj",
+  },
+  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+];
 
 const Stores = () => {
   return (
@@ -31,22 +67,35 @@ const Stores = () => {
           </p>
         </div>
 
-        <div className="actions">
+        <div className="button-actions">
           <div className="buttons">
-            <Button className=" button button-create-new">
-              <Add /> Create new
-            </Button>
-            <Button className="button button-import-csv"> Import CSV</Button>
-            <Button className="button button-bulk-edit">Bulk Edit</Button>
+            <div className="first">
+              <Button className=" button button-create-new">
+                <Add /> Create new
+              </Button>
+              <Button className="button button-import-csv"> Import CSV</Button>
+              <Button className="button button-bulk-edit">Bulk Edit</Button>
+            </div>
+
+            <div className="filter">
+              <Button>
+                {" "}
+                <FilterAlt className="filter-alt" />
+                Filter{" "}
+              </Button>
+              <input placeholder="Type and hit enter" />
+              <Loop />
+            </div>
           </div>
 
-          <div className="filter">
-            <Button>
-              {" "}
-              <FilterAlt />
-              Filter{" "}
-            </Button>
-            <input placeholder="Type and hit enter" />
+          <div className="table" style={{ height: 400, width: "100%" }}>
+            <DataGrid
+              rows={rows}
+              columns={columns}
+              pageSize={5}
+              rowsPerPageOptions={[7]}
+              checkboxSelection
+            />
           </div>
         </div>
       </div>
